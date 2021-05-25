de3073013c95a2e33c4c8370798e34a28235882a
darteyobeng <78813773+darteyobeng@users.noreply.github.com>
2021-05-25 06:13:10 +0000

    changes again

--- Object_Detection.py
+++ Object_Detection.py
@@ -4,7 +4,7 @@ import numpy as np
 
 net = cv2.dnn.readNet('yolov3.weights','yolov3-416.cfg')
 classes = []
-with open('name_file', 'r') as f:
+with open('name_file.names', 'r') as f:
     classes = f.read().splitlines()
     
     
